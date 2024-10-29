31767067335f374c65ee7e4b8f05a2a90abd0e7d
Jeffrey 2 <jif8k@hotmail.com>
2024-10-28 20:30:29 -0400

    Hello world

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/HelloWorld.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.tuning;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Drawing;
+
+public class HelloWorld extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
+
+        waitForStart();
+
+        telemetry.addData("Hello", "World");
+
+        while (opModeIsActive()) {
+
+        }
+
+    }
+}
