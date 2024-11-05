3a9db69303bffa9df7572f566cd19c0e624a742e
Jeffrey 2 <jif8k@hotmail.com>
2024-11-04 22:48:39 -0500

    tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -63,32 +63,32 @@ public final class MecanumDrive {
                 RevHubOrientationOnRobot.UsbFacingDirection.RIGHT;
 
         // drive model parameters
-        public double inPerTick = 0.0279486728774829;
-        public double lateralInPerTick = 0.0273513095084505;
+        public double inPerTick = 0.0231696995344901;
+        public double lateralInPerTick = 0.0228513095084505;
         public double trackWidthTicks = 922.6943228960521;
 
         // feedforward parameters (in tick units)
         public double kS = 0.7463453105505442;
         public double kV = 0.004149425258238021;
-        public double kA = 0.015;
+        public double kA = 0.00001;
 
         // path profile parameters (in inches)
-        public double maxWheelVel = 50;
-        public double minProfileAccel = -30;
-        public double maxProfileAccel = 50;
+        public double maxWheelVel = 10;
+        public double minProfileAccel = -6;
+        public double maxProfileAccel = 10;
 
         // turn profile parameters (in radians)
         public double maxAngVel = Math.PI; // shared with path
         public double maxAngAccel = Math.PI;
 
         // path controller gains
-        public double axialGain = 0.0;
-        public double lateralGain = 0.0;
-        public double headingGain = 0.0; // shared with turn
+        public double axialGain = 4.0;
+        public double lateralGain = 3.0;
+        public double headingGain = 2.0; // shared with turn
 
         public double axialVelGain = 0.0;
         public double lateralVelGain = 0.0;
-        public double headingVelGain = 0.0; // shared with turn
+        public double headingVelGain = 0.1; // shared with turn
     }
 
     public static Params PARAMS = new Params();
