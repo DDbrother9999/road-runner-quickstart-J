2fd7326af2d981f77707bf148e5e6068aa002eba
JeffreyL-3 <boby8k@gmail.com>
2024-12-25 18:54:12 -0500

    testrr

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestRR.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+// RR-specific imports
+import com.acmerobotics.dashboard.config.Config;
+        import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+        import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+
+// Non-RR imports
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+        import org.firstinspires.ftc.teamcode.tuning.TuningOpModes;
+
+@Config
+@Autonomous(name = "TestRR", group = "Autonomous")
+
+public final class TestRR extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Pose2d beginPose = new Pose2d(0, 0, 0);
+        if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
+            MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+            Action simpleSpline = drive.actionBuilder(beginPose)
+                    .splineTo(new Vector2d(30, 30), Math.PI / 2)
+                    .splineTo(new Vector2d(0, 60), Math.PI)
+                    .build();
+            waitForStart();
+            Actions.runBlocking(
+                    simpleSpline);
+        }
+        else {
+            throw new RuntimeException();
+        }
+    }
+}
+
