a7c7fc3a07426af1f878e0c61fe5aa8a563fb5a2
JeffreyL-3 <boby8k@gmail.com>
2024-12-27 23:01:26 -0500

    meep update

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -16,15 +16,28 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-
-                .splineTo(new Vector2d(30, 22), 0)
-                .strafeTo(new Vector2d(0, 0))
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, Math.toRadians(90)))
+                .lineToYSplineHeading(33, Math.toRadians(0))
+                .waitSeconds(2)
+                .setTangent(Math.toRadians(90))
+                .lineToY(48)
+                .setTangent(Math.toRadians(0))
+                .lineToX(32)
+                .strafeTo(new Vector2d(44.5, 30))
+                .turn(Math.toRadians(180))
+                .lineToX(47.5)
+                .waitSeconds(3)
                 .build());
-
 
                 /*
 
+                .setTangent(0)
+                .splineToConstantHeading(new Vector2d(48, 48), Math.toRadians(60))
+                .build());
+
+                */
+
+                /*
                 .lineToY(-34)
                 .turn(Math.toRadians(-90))
                 .lineToX(30)
