fd685df03536d0d3982c06661ddc4d576dcf0b04
Jeffrey <boby8k@gmail.com>
2025-01-16 10:00:24 -0500

    Complete DistanceSensor class (not telemetry yet)

--- TeamCode/src/main/java/edu/nobles/robotics/sensors/DistanceSensor.java
+++ TeamCode/src/main/java/edu/nobles/robotics/sensors/DistanceSensor.java
@@ -1,14 +1,43 @@
 package edu.nobles.robotics.sensors;
 
-import com.arcrobotics.ftclib.hardware.SensorRevTOFDistance;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import androidx.annotation.NonNull;
 
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.arcrobotics.ftclib.hardware.SensorRevTOFDistance;
+import com.arcrobotics.ftclib.hardware.SimpleServo;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.RobotLog;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+@Config
 public class DistanceSensor{
 
-    private SensorRevTOFDistance distanceSensor;
+    private final SensorRevTOFDistance distanceSensor;
+    private final String deviceName;
+    private final Telemetry telemetry;
 
-    public DistanceSensor(SensorRevTOFDistance distanceSensor){
+    public DistanceSensor(String deviceName, SensorRevTOFDistance distanceSensor, Telemetry telemetry){
         this.distanceSensor=distanceSensor;
+        this.deviceName = deviceName;
+        this.telemetry = telemetry;
     }
 
+
+    public String getDeviceName() {
+        return deviceName;
+    }
+
+    /**
+     * Unit is CM
+     */
+    public double getDistance() {
+        return distanceSensor.getDistance(DistanceUnit.CM);
+    }
 }
+
