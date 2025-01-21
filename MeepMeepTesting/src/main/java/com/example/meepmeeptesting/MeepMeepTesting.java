01ea01525c694f86e90c14004bddc47929cadb3c
Jeffrey <boby8k@gmail.com>
2025-01-20 19:52:52 -0500

    Auto testing

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -16,21 +16,25 @@ public class MeepMeepTesting {
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, Math.toRadians(90)))
-                .lineToYSplineHeading(33, Math.toRadians(0))
-                .waitSeconds(2)
-                .setTangent(Math.toRadians(90))
-                .lineToY(48)
-                .setTangent(Math.toRadians(0))
-                .lineToX(32)
-                .strafeTo(new Vector2d(44.5, 30))
-                .turn(Math.toRadians(180))
-                .lineToX(47.5)
-                .waitSeconds(3)
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, -63, Math.toRadians(90)))
+                .strafeTo(new Vector2d(0, -43))
+                //Check for vertical slide extension
+                .strafeTo(new Vector2d(0, -34))
+                //Retract vertical slide, snapping piece onto
+                .strafeTo(new Vector2d(36, -34))
+                .strafeTo(new Vector2d(36, 0))
+                .strafeTo(new Vector2d(47, 0))
+                .strafeTo(new Vector2d(47, -40)) //Drops off in observation zone
+                .strafeTo(new Vector2d(47, 0))
+                .strafeTo(new Vector2d(56, 0))
+                .strafeTo(new Vector2d(56, -40))
+                .strafeTo(new Vector2d(56, 0))
+                .strafeTo(new Vector2d(56, 0))
+
                 .build());
 
                 /*
-
+s
                 .setTangent(0)
                 .splineToConstantHeading(new Vector2d(48, 48), Math.toRadians(60))
                 .build());
