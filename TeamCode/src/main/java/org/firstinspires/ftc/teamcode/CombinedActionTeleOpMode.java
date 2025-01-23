3dd5d862e700785558515115b4f6d36503bc3f82
Jeffrey <boby8k@gmail.com>
2025-01-23 00:14:43 -0500

    claw works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CombinedActionTeleOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CombinedActionTeleOpMode.java
@@ -18,6 +18,8 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.RobotLog;
 
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -44,13 +46,12 @@ public class CombinedActionTeleOpMode extends LinearOpMode {
     // if you don't rotate in steps, set it to large number, such as 400
     public static double flip0_oneStepRotationInDegree = 400;
 
-    public static double claw1_openDegree = 0;
-    public static double claw1_closeDegree = 30;
-    public static double claw2_openDegree = 30;
-    public static double claw2_closeDegree = 0;
+    public static double claw1_openDegree = 95;
+    public static double claw1_closeDegree = 84;
+    public static double claw2_openDegree = 128;
+    public static double claw2_closeDegree = 143;
 
-    public static double contRotateServo_power = 0.25;
-
+    public static double intake1Spin_power = 0.25;
 
     public static int vertUp_max = 2000;
     public static double vertUp_maxPower = 0.25;
@@ -71,18 +72,19 @@ public class CombinedActionTeleOpMode extends LinearOpMode {
 
     private boolean flipFlat = false;
     private boolean clawOpen = true;
-    private boolean contRotateStoped = true;
 
     private MecanumDrive mecanumDrive;
     private ServoDevice flipServo;
     private ServoDevice clawServo1;
     private ServoDevice clawServo2;
-    private CRServo contRotateServo;
+    private CRServo intake1SpinServo;
     private ServoDevice servoArmSpinner;
     private SlideMotor vertSlideUp;
     private SlideMotor vertSlideDown;
     private GamepadEx gamepadEx1;
     private GamepadEx gamepadEx2;
+
+    private final List<String> unavailableHardwares = new ArrayList<>();
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -95,7 +97,6 @@ public class CombinedActionTeleOpMode extends LinearOpMode {
         }
 
         initHardware();
-        telemetry.update();
 
         waitForStart();
 
@@ -136,24 +137,29 @@ public class CombinedActionTeleOpMode extends LinearOpMode {
                 addActionEx(vertSlideUp.moveSlide(vertUp_targetDown, vertUp_maxPower));
                 addActionEx(vertSlideDown.moveSlide(vertDown_targetDown, vertDown_maxPower));
             }
-            if (gamepadEx1.wasJustPressed(GamepadKeys.Button.LEFT_BUMPER) && clawServo1.available && clawServo2.available) {
+            if (gamepadEx1.wasJustPressed(GamepadKeys.Button.A)) {
                 if (clawOpen) {
-                    clawServo1.servo.turnToAngle(claw1_closeDegree);
-                    clawServo2.servo.turnToAngle(claw2_closeDegree);
+                    if (clawServo1.available)
+                        clawServo1.servo.turnToAngle(claw1_closeDegree, AngleUnit.DEGREES);
+                    if (clawServo2.available)
+                        clawServo2.servo.turnToAngle(claw2_closeDegree, AngleUnit.DEGREES);
                 } else {
-                    clawServo1.servo.turnToAngle(claw1_openDegree);
-                    clawServo2.servo.turnToAngle(claw2_openDegree);
+                    if (clawServo1.available)
+                        clawServo1.servo.turnToAngle(claw1_openDegree, AngleUnit.DEGREES);
+                    if (clawServo2.available)
+                        clawServo2.servo.turnToAngle(claw2_openDegree, AngleUnit.DEGREES);
                 }
                 clawOpen = !clawOpen;
             }
 
-            if (gamepadEx1.wasJustPressed(GamepadKeys.Button.RIGHT_BUMPER) && contRotateServo != null) {
-                if (contRotateStoped) {
-                    contRotateServo.set(contRotateServo_power);
+            if(intake1SpinServo != null) {
+                if (gamepadEx1.isDown(GamepadKeys.Button.RIGHT_BUMPER) ) {
+                    intake1SpinServo.set(intake1Spin_power);
+                } else if(gamepadEx1.isDown(GamepadKeys.Button.LEFT_BUMPER)){
+                    intake1SpinServo.set(-intake1Spin_power);
                 } else {
-                    contRotateServo.set(0);
+                    intake1SpinServo.set(0);
                 }
-                contRotateStoped = !contRotateStoped;
             }
 
             // update running actions
@@ -176,17 +182,18 @@ public class CombinedActionTeleOpMode extends LinearOpMode {
         List<ServoDevice> servoList = new ArrayList<>();
 
         mecanumDrive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-        flipServo = new ServoDevice("servoArmFlip", hardwareMap, telemetry);
+        flipServo = new ServoDevice("servo0", hardwareMap, telemetry);
         servoList.add(flipServo);
         //servoArmSpinner = new ServoDevice("servoArmSpinner", hardwareMap, telemetry);
-        clawServo1 = new ServoDevice("claw1", hardwareMap, telemetry);
+        clawServo1 = new ServoDevice("servo1", hardwareMap, telemetry);
         servoList.add(clawServo1);
-        clawServo2 = new ServoDevice("claw2", hardwareMap, telemetry);
+        clawServo2 = new ServoDevice("servo3", hardwareMap, telemetry);
         servoList.add(clawServo2);
         try {
-            contRotateServo = new CRServo(hardwareMap, "contRotate");
+            intake1SpinServo = new CRServo(hardwareMap, "servo5");
+            intake1SpinServo.setInverted(true);
         } catch (Exception e) {
-            RobotLog.e("Continuous Rotate Servo is not available");
+            RobotLog.e("intake1SpinServo is not available");
         }
 
         try {
@@ -221,7 +228,6 @@ public class CombinedActionTeleOpMode extends LinearOpMode {
         gamepadEx1 = new GamepadEx(gamepad1);
         gamepadEx2 = new GamepadEx(gamepad2);
 
-        List<String> unavailableHardwares = new ArrayList<>();
         if (!mecanumDrive.available) {
             unavailableHardwares.add("MecanumDrive");
         }
@@ -231,16 +237,17 @@ public class CombinedActionTeleOpMode extends LinearOpMode {
         if (vertSlideDown == null) {
             unavailableHardwares.add("vertSlideDown");
         }
-        if (contRotateServo == null) {
-            unavailableHardwares.add("contRotateServo");
+        if (intake1SpinServo == null) {
+            unavailableHardwares.add("intake1SpinServo");
         }
         unavailableHardwares.addAll(
                 servoList.stream().filter(servo -> !servo.available).map(ServoDevice::getDeviceName).collect(Collectors.toList()));
-
-        telemetry.addData("Unavailable devices", String.join(", ", unavailableHardwares));
     }
 
     private void vertSlideControl() {
+        if (vertSlideUp == null || vertSlideDown == null)
+            return;
+
         boolean moving = false;
         float upPower = -gamepad1.left_stick_y;
         if (upPower > 0) {
@@ -266,13 +273,10 @@ public class CombinedActionTeleOpMode extends LinearOpMode {
             vertSlideUp.slideMotor.set(0);
             vertSlideDown.slideMotor.set(0);
         }
-        telemetry.addData("vertSlideUp position: ", vertSlideUp.slideMotor.getCurrentPosition());
-        telemetry.addData("vertSlideDown position: ", vertSlideDown.slideMotor.getCurrentPosition());
     }
 
     private void report(MecanumDrive drive, TelemetryPacket packet) {
-        telemetry.addData("vertSlide up position", vertSlideUp.slideMotor.getCurrentPosition());
-        telemetry.addData("vertSlide down position", vertSlideDown.slideMotor.getCurrentPosition());
+        telemetry.addData("Unavailable devices", String.join(", ", unavailableHardwares));
 
         telemetry.addData("left_stick_y", gamepad1.left_stick_y);
         telemetry.addData("left_stick_x", gamepad1.left_stick_x);
@@ -284,6 +288,30 @@ public class CombinedActionTeleOpMode extends LinearOpMode {
             telemetry.addData("y", drive.pose.position.y);
             telemetry.addData("heading (deg)", Math.toDegrees(drive.pose.heading.toDouble()));
         }
+
+        if (vertSlideUp != null) {
+            telemetry.addData("vertSlideUp position", vertSlideUp.slideMotor.getCurrentPosition());
+        }
+        if (vertSlideDown != null) {
+            telemetry.addData("vertSlideDown position", vertSlideDown.slideMotor.getCurrentPosition());
+        }
+
+        if (intake1SpinServo != null) {
+            telemetry.addData("intake1Spin power", intake1SpinServo.get());
+        }
+
+        if (clawServo1.available) {
+            telemetry.addData("clawServo1", clawServo1.getAngle());
+        }
+
+        if (clawServo2.available) {
+            telemetry.addData("clawServo2", clawServo2.getAngle());
+        }
+
+        if(flipServo.available) {
+            telemetry.addData("flipServo", flipServo.getAngle());
+        }
+
         telemetry.update();
 
         packet.fieldOverlay().setStroke("#3F51B5");
