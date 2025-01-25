59066bdc9713d321dc5e2a77ef891da296029d1b
Jeffrey <boby8k@gmail.com>
2025-01-24 21:14:03 -0500

    better basic motor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleBasicMotor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SimpleBasicMotor.java
@@ -1,16 +1,21 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@Config
 @TeleOp
 public class SimpleBasicMotor extends LinearOpMode {
+    public static String MotorName = "vertSlideRightDown";
     @Override
     public void runOpMode() throws InterruptedException {
+
+
         // Declare our motors
         // Make sure your ID's match your configuration
-        DcMotor motorSlide0 = hardwareMap.dcMotor.get("backLeft");
+        DcMotor motorSlide0 = hardwareMap.dcMotor.get(MotorName);
 
         waitForStart();
 
