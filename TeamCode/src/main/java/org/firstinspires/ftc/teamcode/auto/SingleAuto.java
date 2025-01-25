5a5ef0895da467783a1e459d67016af0a4758ad2
Jeffrey <boby8k@gmail.com>
2025-01-24 21:14:31 -0500

    auto (not working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/SingleAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/SingleAuto.java
@@ -1,5 +1,16 @@
 package org.firstinspires.ftc.teamcode.auto;
 
+import static edu.nobles.robotics.DeviceNameList.clawLeftName;
+import static edu.nobles.robotics.DeviceNameList.clawRightName;
+import static edu.nobles.robotics.DeviceNameList.intake1FlipName;
+import static edu.nobles.robotics.DeviceNameList.intake1SlideExtendName;
+import static edu.nobles.robotics.DeviceNameList.intake1SlideRetractName;
+import static edu.nobles.robotics.DeviceNameList.intake1spinName;
+import static edu.nobles.robotics.DeviceNameList.vertSlideLeftDownName;
+import static edu.nobles.robotics.DeviceNameList.vertSlideLeftUpName;
+import static edu.nobles.robotics.DeviceNameList.vertSlideRightDownName;
+import static edu.nobles.robotics.DeviceNameList.vertSlideRightUpName;
+
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
@@ -8,11 +19,13 @@ import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.arcrobotics.ftclib.hardware.motors.CRServo;
 import com.arcrobotics.ftclib.hardware.motors.Motor;
 import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.util.RobotLog;
 
 import org.firstinspires.ftc.teamcode.CombinedActionTeleOpMode;
@@ -22,6 +35,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import edu.nobles.robotics.motor.HorizontalExtender;
 import edu.nobles.robotics.motor.MotorGroupEx;
 import edu.nobles.robotics.motor.SlideMotor;
 import edu.nobles.robotics.servo.ServoDevice;
@@ -47,21 +61,15 @@ public class SingleAuto extends LinearOpMode {
     //start pose
     private Pose2d beginPose = new Pose2d(0, -62.5, Math.toRadians(90));
     private intake1 testIntake1 = new intake1(hardwareMap);
-    private MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
 
     private MecanumDrive mecanumDrive;
-    private ServoDevice flipServo;
-    private ServoDevice clawServo1;
-    private ServoDevice clawServo2;
-    private CRServo contRotateServo;
-    private ServoDevice servoArmSpinner;
-    private SlideMotor vertSlideUp;
-    private SlideMotor vertSlideDown;
 
 
     @Override
     public void runOpMode() {
-        initHardeware();
+
+        mecanumDrive = new MecanumDrive(hardwareMap, beginPose);
 
         TrajectoryActionBuilder moveInitialTraj = mecanumDrive.actionBuilder(beginPose)
                 .strafeTo(new Vector2d(0, -43))
@@ -97,84 +105,18 @@ public class SingleAuto extends LinearOpMode {
         Actions.runBlocking(
                 new SequentialAction(
                         new ParallelAction(
-                                moveInitial,
-                                vertSlideUp.moveSlide(CombinedActionTeleOpMode.vertUp_targetUp, CombinedActionTeleOpMode.vertUp_maxPower),
-                                vertSlideDown.moveSlide(CombinedActionTeleOpMode.vertDown_targetUp, CombinedActionTeleOpMode.vertDown_maxPower)
+                                moveInitial
+                                //vertSlideUp.moveSlide(CombinedActionTeleOpMode.vertUp_targetUp, CombinedActionTeleOpMode.vertUp_maxPower),
+                                //vertSlideDown.moveSlide(CombinedActionTeleOpMode.vertDown_targetUp, CombinedActionTeleOpMode.vertDown_maxPower)
                         ),
                         moveToSub,
                         new ParallelAction(
-                                clawServo1.rotateNormal(CombinedActionTeleOpMode.claw1_openDegree),
-                                clawServo2.rotateNormal(CombinedActionTeleOpMode.claw2_openDegree)
+                                //clawServo1.rotateNormal(CombinedActionTeleOpMode.claw1_openDegree),
+                                //clawServo2.rotateNormal(CombinedActionTeleOpMode.claw2_openDegree)
                         ),
                         push
                 )
         );
-    }
-
-    private void initHardeware() {
-        List<ServoDevice> servoList = new ArrayList<>();
-
-        mecanumDrive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
-        flipServo = new ServoDevice("servoArmFlip", hardwareMap, telemetry);
-        servoList.add(flipServo);
-        //servoArmSpinner = new ServoDevice("servoArmSpinner", hardwareMap, telemetry);
-        clawServo1 = new ServoDevice("claw1", hardwareMap, telemetry);
-        servoList.add(clawServo1);
-        clawServo2 = new ServoDevice("claw2", hardwareMap, telemetry);
-        servoList.add(clawServo2);
-        try {
-            contRotateServo = new CRServo(hardwareMap, "contRotate");
-        } catch (Exception e) {
-            RobotLog.e("Continuous Rotate Servo is not available");
-        }
-
-        try {
-            MotorEx vertSlideLeftUp = new MotorEx(hardwareMap, "vertSlideLeftUp", Motor.GoBILDA.RPM_435);
-            vertSlideLeftUp.setInverted(true);
-            MotorEx vertSlideRightUp = new MotorEx(hardwareMap, "vertSlideRightUp", Motor.GoBILDA.RPM_435);
-
-            //DON'T INVERT MOTORS AFTER HERE
-            MotorGroupEx vertSlideUpGroup = new MotorGroupEx(vertSlideRightUp, vertSlideLeftUp);
-
-            vertSlideUp = new SlideMotor(vertSlideUpGroup, telemetry, "vertSlideUp");
-            vertSlideUp.setManualMode();
-        } catch (Exception e) {
-            RobotLog.e("VertSlideUp are not available");
-        }
-
-        try {
-            MotorEx vertSlideLeftDown = new MotorEx(hardwareMap, "vertSlideLeftDown", Motor.GoBILDA.RPM_312);
-            vertSlideLeftDown.setInverted(true);
-            MotorEx vertSlideRightDown = new MotorEx(hardwareMap, "vertSlideRightDown", Motor.GoBILDA.RPM_312);
-
-            //DON'T INVERT MOTORS AFTER HERE
-            MotorGroupEx vertSlideDownGroup = new MotorGroupEx(vertSlideRightDown, vertSlideLeftDown);
-
-            vertSlideDown = new SlideMotor(vertSlideDownGroup, telemetry, "vertSlideDown");
-            vertSlideDown.setManualMode();
-        } catch (Exception e) {
-            RobotLog.e("VertSlideDown are not available");
-        }
-
-        //NO GAMEPADS
-
-        List<String> unavailableHardwares = new ArrayList<>();
-        if (!mecanumDrive.available) {
-            unavailableHardwares.add("MecanumDrive");
-        }
-        if (vertSlideUp == null) {
-            unavailableHardwares.add("vertSlideUp");
-        }
-        if (vertSlideDown == null) {
-            unavailableHardwares.add("vertSlideDown");
-        }
-        if (contRotateServo == null) {
-            unavailableHardwares.add("contRotateServo");
-        }
-        unavailableHardwares.addAll(
-                servoList.stream().filter(servo -> !servo.available).map(ServoDevice::getDeviceName).collect(Collectors.toList()));
-
-        telemetry.addData("Unavailable devices", String.join(", ", unavailableHardwares));
     }
 
 }
