7ff2d883d8978b8187761624b6b95abd92ca824e
Jeffrey <boby8k@gmail.com>
2025-01-31 16:46:07 -0500

    initial v2 device names + ports

--- TeamCode/src/main/java/edu/nobles/robotics/DeviceNameList.java
+++ TeamCode/src/main/java/edu/nobles/robotics/DeviceNameList.java
@@ -2,23 +2,23 @@ package edu.nobles.robotics;
 
 import com.acmerobotics.dashboard.config.Config;
 
+//Key: if not specified, is on control hub
+
 @Config
 public class DeviceNameList {
     // Servos
-    public static String clawLeftName = "servoExp0";
-    public static String clawRightName = "servoExp1";
-    public static String intake1SlideRetractName = "servoExp3";
-    public static String intake1SlideExtendName = "servoExp5";
+    public static String clawName = "servoExp0";
+
     public static String intake1FlipName = "servo3";
     public static String intake1spinName = "servo5";
 
     // Motors
-    public static String frontLeftName = "frontLeft";
-    public static String backLeftName = "backLeft";
-    public static String backRightName = "backRight";
-    public static String frontRightName = "frontRight";
-    public static String vertSlideRightUpName = "vertSlideRightUp";
-    public static String vertSlideRightDownName = "vertSlideRightDown";
-    public static String vertSlideLeftUpName = "vertSlideLeftUp";
-    public static String vertSlideLeftDownName = "vertSlideLeftDown";
+    public static String intake1SlideExtendName = "motor3"; //gobilda 435
+
+    public static String frontLeftName = "motor1";
+    public static String backLeftName = "motor2";
+    public static String backRightName = "motorExp1";
+    public static String frontRightName = "motorExp3";
+    public static String vertSlideUpName = "motorExp0"; //gobilda 435
+    public static String vertSlideDownName = "motor0"; //gobilda 312
 }
